14c26c20ec3154bb7c735a581670857b025b9a2f
Duru Ercevik <duru.ercevik@epitech.eu>
2026-09-23 12:02:46 +0300

    challenge uploaded.
    Signed-off-by: Duru Ercevik <duru.ercevik@epitech.eu>

---
+++ day03/challenge.py
@@ -0,0 +1,7 @@
+text = input("Enter a string: ")
+
+text = text.lower()
+
+total = (text.count("cat") + text.count("tac") + text.count("garden") + text.count("nedrag") + text.count("mice") + text.count("ecim"))
+
+print(total)
