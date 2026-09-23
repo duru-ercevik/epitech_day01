88facd4c740de5cb496b739b175c650fa067767a
Duru Ercevik <duru.ercevik@epitech.eu>
2026-09-23 12:14:05 +0300

    task3.3 uploaded.
    Signed-off-by: Duru Ercevik <duru.ercevik@epitech.eu>

---
+++ day03/task3.3.py
@@ -0,0 +1,8 @@
+first_input=input("type the first number:")
+second_input=input("type the second number:")
+
+first_number=int(first_input)
+second_number=int(second_input)
+
+total=first_number+second_number
+print("the total number is:" , total)
