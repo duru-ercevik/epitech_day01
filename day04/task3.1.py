4186c6802f74d30cc01a2e7aeb3701ba0a0c5ab1
Duru Ercevik <duru.ercevik@epitech.eu>
2026-09-24 12:40:56 +0300

    task3.1 is uploaded.
    Signed-off-by: Duru Ercevik <duru.ercevik@epitech.eu>

---
+++ day04/task3.1.py
@@ -0,0 +1,15 @@
+user_text=input("enter a text:")
+key=int(input("enter the hey value:"))
+
+encrypted = ""
+
+for char in user_text:
+    if char.isalpha(): #to check if it is a letter or not
+        position = ord(char.lower()) - ord("a") #to find the location in the alphabet we need to subtract from a (starting point)
+        new_position = (position + key) % 26 #finding the new position
+        new_char = chr(new_position + ord("a")) #turning it into character
+        encrypted += new_char #adding it to our string
+    else:
+        encrypted += char #do not change if it space or soething else than letters
+
+print(encrypted)
