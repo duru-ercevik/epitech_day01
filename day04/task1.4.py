3a76a7dc55fe23d995044a0aa5c7cfc140b9ea14
Duru Ercevik <duru.ercevik@epitech.eu>
2026-09-24 11:33:27 +0300

    task1.4 is uploaded.
    Signed-off-by: Duru Ercevik <duru.ercevik@epitech.eu>

---
+++ day04/task1.4.py
@@ -0,0 +1,7 @@
+user_text=input("enter an input:")
+if user_text=="open sesame":
+    print("access granted")
+elif user_text=="will you open, you goddamn !@&/°":
+    print("access fucking granted")
+else:
+    print("permission denied")
