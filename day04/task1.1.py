5e64a4ed08d1bdc0e9e52dfe2908a0e18e452253
Duru Ercevik <duru.ercevik@epitech.eu>
2026-09-24 11:25:16 +0300

    task1.1 is uploaded.
    Signed-off-by: Duru Ercevik <duru.ercevik@epitech.eu>

---
+++ day04/task1.1.py
@@ -0,0 +1,8 @@
+(42 > 12) #returns true because 42 is bigger than 12
+(12 = 12) #syntax error because = means assigning a value
+(12 == 12) #returns true
+(”hello” == ”world”) #returns false
+(218 >= 118) #returns true
+(”a”.upper() == ”A”) #returns true
+(1 ∗ 2 ∗ 3 ∗ 4 <= 9) #returns false
+(”z” in ”azerty”) #returns true
