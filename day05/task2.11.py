4cb65e235c3ed98e950b230d3667a603d33493b6
Duru Ercevik <duru.ercevik@epitech.eu>
2026-09-25 14:51:55 +0300

    task2.11 is uploaded.
    Signed-off-by: Duru Ercevik <duru.ercevik@epitech.eu>

---
+++ day05/task2.11.py
@@ -0,0 +1,13 @@
+numbers = {
+    "dalmatians": 101,
+    "pi": 3.14,
+    "beast": 666,
+    "life": 42,
+    "googol": 10 ** 100,
+    "jordan": 23,
+    "life, the universe and everything": 42,
+    "emergency": 911,
+    "euler": 2.71828,
+}
+print(max(numbers, key=numbers.get))
+ 
