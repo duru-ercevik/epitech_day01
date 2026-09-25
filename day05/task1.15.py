a57f22fbe4dd8c69eebccb547f925098cbdd4ad5
Duru Ercevik <duru.ercevik@epitech.eu>
2026-09-25 12:51:50 +0300

    task1.15 is uploaded.
    Signed-off-by: Duru Ercevik <duru.ercevik@epitech.eu>

---
+++ day05/task1.15.py
@@ -0,0 +1,2 @@
+print([x // 2 if x % 2 == 0 else x * 2 for x in [42, 3, 4, 18, 3, 10]]) 
+#for every element in the list check if the element is divisible by 2 if it is divide it but if it is not multiply it by 2
