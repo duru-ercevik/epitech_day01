94fc34ed82fe290f810d58e1b5bb01596fcddc4b
Duru Ercevik <duru.ercevik@epitech.eu>
2026-09-25 14:28:55 +0300

    task2.5 is uploaded.
    Signed-off-by: Duru Ercevik <duru.ercevik@epitech.eu>

---
+++ day05/task2.5.py
@@ -0,0 +1,9 @@
+types = {"Electric": [], "Grass": [], "Fire": []}
+
+types["Electric"].append("Pikachu")
+types["Grass"].append("Bulbasaur")
+types["Fire"].append("Charmander")
+types["Grass"].append("Leafeaon")
+types["Grass"].append("Scovillain")
+types["Fire"].append("Scovillain")
+print(types)
