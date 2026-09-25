18908cc5574df94f3aeceea08faf727ff47ce22e
Duru Ercevik <duru.ercevik@epitech.eu>
2026-09-25 14:24:38 +0300

    task2.3 is uploaded.
    Signed-off-by: Duru Ercevik <duru.ercevik@epitech.eu>

---
+++ day05/task2.3.py
@@ -0,0 +1,6 @@
+pokemons = {"Pikachu": "Electric", "Bulbasaur": "Grass", "Charmander": "Fire"}
+
+pokemons["Pikachu"] = ["Pichu", "Raichu"]
+print(pokemons)
+
+#the key value is unique in the dictionary so the old value is gone and pikachu is matched with the new thing
